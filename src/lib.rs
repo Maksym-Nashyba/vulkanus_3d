ec21b8dfc01ba06cafe037f3455b0eb3f61a1c03
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-04-11 16:35:28 +0200

    exposed a memory allocator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::sync::Arc;
+use vulkano::device::Device;
+use vulkano::memory::allocator::{AllocationType, GenericMemoryAllocatorCreateInfo, StandardMemoryAllocator};
 use winit::window::Window;
 use crate::renderer::Renderer;
 
@@ -7,4 +9,8 @@ pub mod material;
 
 pub fn innit_renderer(window:Arc<Window>) -> Renderer{
     return renderer::initialize_renderer(window);
+}
+
+pub fn new_allocator(device:Arc<Device>) -> StandardMemoryAllocator{
+    return StandardMemoryAllocator::new_default(device);
 }
