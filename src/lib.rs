b7db1fd8c53e78f982ee05305ad896fd75c86cb4
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-04-13 12:02:45 +0200

    small fix

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ use crate::renderer::Renderer;
 pub mod renderer;
 pub mod material;
 
-pub fn innit_renderer(window:Arc<Window>) -> Renderer{
-    return Renderer::new(window);
+pub fn innit_renderer(window:Arc<Window>, present_immediate:bool) -> Renderer{
+    return Renderer::new(window, present_immediate);
 }
